4982df20c5a7a6b5acccfdf6ec8b5565184ff3e3
AegonZF <pablofz2002@gmail.com>
2025-12-03 11:08:41 -0600

    feature: Image picker

--- lib/Screens/Home Screen/create_poll_screen.dart
+++ lib/Screens/Home Screen/create_poll_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CreatePollScreen extends StatefulWidget {
   const CreatePollScreen({super.key});
@@ -14,6 +17,8 @@ class _CreatePollScreenState extends State<CreatePollScreen> {
     TextEditingController(),
     TextEditingController(),
   ];
+  final List<File?> optionImages = [null, null, null];
+  final ImagePicker _imagePicker = ImagePicker();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,9 +29,11 @@ class _CreatePollScreenState extends State<CreatePollScreen> {
           children: [
             Container(
               margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+
               decoration: BoxDecoration(
                 color: Colors.white,
-                borderRadius: BorderRadius.circular(5),
+                borderRadius: BorderRadius.circular(10),
                 border: Border.all(color: Colors.grey.withOpacity(0.2)),
                 boxShadow: [
                   BoxShadow(
@@ -45,9 +52,10 @@ class _CreatePollScreenState extends State<CreatePollScreen> {
             for (int i = 0; i < 3; i++)
               Container(
                 margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
                 decoration: BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.circular(5),
+                  borderRadius: BorderRadius.circular(10),
                   border: Border.all(color: Colors.grey.withOpacity(0.2)),
                   boxShadow: [
                     BoxShadow(
@@ -68,6 +76,31 @@ class _CreatePollScreenState extends State<CreatePollScreen> {
                         ),
                       ),
                     ),
+                    SizedBox(width: 15),
+                    GestureDetector(
+                      onTap: () {
+                        pickImage(i);
+                      },
+                      child: optionImages[i] != null
+                          ? ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.file(
+                                optionImages[i]!,
+                                height: 50,
+                                width: 50,
+                                fit: BoxFit.cover,
+                              ),
+                            )
+                          : Container(
+                              decoration: BoxDecoration(
+                                color: Colors.grey[300],
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              height: 50,
+                              width: 50,
+                              child: Icon(Icons.image),
+                            ),
+                    ),
                   ],
                 ),
               ),
@@ -75,5 +108,15 @@ class _CreatePollScreenState extends State<CreatePollScreen> {
         ),
       ),
     );
+  }
+
+  Future<void> pickImage(int i) async {
+    final pickedFile = await _imagePicker.pickImage(
+      source: ImageSource.gallery,
+    );
+    if (pickedFile != null) {
+      optionImages[i] = File(pickedFile.path);
+      setState(() {});
+    }
   }
 }
